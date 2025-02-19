6d731209756a494e8fc1140edc7109225cc6416d
Tibo De Peuter <tibo.depeuter@ugent.be>
2025-02-19 18:34:16 +0100

    chore(frontend): Add Vuetify
    Add Vuetify and simple slider to about page

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,24 @@
 import './assets/main.css'
+import { createApp } from 'vue';
 
-import { createApp } from 'vue'
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+// Components
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+app.use(vuetify);
+
+app.mount('#app');
