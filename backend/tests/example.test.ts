db4d6288d88380b3a5ed21fbeeabe05dd5d0b993
Tibo De Peuter <tibo.depeuter@ugent.be>
2025-02-20 14:02:33 +0100

    chore: Configureer vitest
    Configuratie en voorbeeldtesten

---
+++ backend/tests/example.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest";
+
+describe("Sample test", () => {
+    it("should sum to 2", () => {
+        const expected = 2;
+        const result = 1 + 1;
+        expect(result).toBe(expected);
+    });
+})
