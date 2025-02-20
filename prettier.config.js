977b49f659b19103ecb2333cf5160d1abd350f06
Tibo De Peuter <tibo.depeuter@ugent.be>
2025-02-20 13:24:37 +0100

    chore: Configueer formatting
    Prettier en .editorconfig

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Options}
+ */
+export default {
+    printWidth: 80,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    objectWrap: 'preserve',
+    bracketSameLine: false,
+    arrowParens: 'always',
+}
