bd93e5fae378fbf036d91a20710ea824f6777253
Gerald Schmittinger <gerald.schmittinger@ugent.be>
2025-02-25 23:18:04 +0100

    feat(backend): ClassRepository & gerelateerde repo's aangemaakt
    ClassRepository, ClassJoinRequestRepository en TeacherInvitationRepository aangemaakt.

---
+++ backend/src/data/classes/class-repository.ts
@@ -0,0 +1,11 @@
+import {DwengoEntityRepository} from "../dwengo-entity-repository";
+import {Class} from "../../entities/classes/class.entity";
+
+export class ClassRepository extends DwengoEntityRepository<Class> {
+    public findById(id: string): Promise<Class | null> {
+        return this.findOne({classId: id});
+    }
+    public deleteById(id: string): Promise<void> {
+        return this.deleteWhere({classId: id});
+    }
+}
