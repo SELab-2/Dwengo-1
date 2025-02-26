bf320c0d33552dee8c51cfde578004ffdab3a4c7
Timo De Meyst <timodemeyst@outlook.com>
2025-02-26 17:58:43 +0100

    Revert "config verder aangepast om te werken met eslint 8.57.1"
    This reverts commit dccabf299cd403e950bc7a405ff5120501f20fca.

--- eslint.config.ts
+++ eslint.config.ts
@@ -14,7 +14,7 @@ export default [
     prettierConfig,
     includeIgnoreFile(gitignorePath),
     {
-        parserOptions: {
+        languageOptions: {
             ecmaVersion: 'latest',
             sourceType: 'module',
         },
