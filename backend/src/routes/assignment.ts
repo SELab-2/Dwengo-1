a7d4868e63e012bf750395b5404769a55ca5a9a5
Adriaan Jacquet <adriaanjacquet@gmail.com>
2025-02-26 14:40:32 +0100

    feat: extra endpoints toegevoegd voor /assignment

--- backend/src/routes/assignment.ts
+++ backend/src/routes/assignment.ts
@@ -9,8 +9,36 @@ router.get('/:id', (req, res) => {
         description: 'Een korte beschrijving',
         groups: [ '0' ],
         learningPath: '0',
-        class: '0'
+        class: '0',
+        links: {
+            self: `${req.baseUrl}/${req.params.id}`,
+            submissions: `${req.baseUrl}/${req.params.id}`,
+        },
     });
 })
+
+router.get('/:id/submissions', (req, res) => {
+    res.json({
+        submissions: [
+            '0'
+        ],
+    });
+});
+
+router.get('/:id/groups', (req, res) => {
+    res.json({
+        groups: [
+            '0'
+        ],
+    });
+});
+
+router.get('/:id/questions', (req, res) => {
+    res.json({
+        questions: [
+            '0'
+        ],
+    });
+});
 
 export default router
