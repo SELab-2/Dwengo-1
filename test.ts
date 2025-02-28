6e0f527b1f9df07a8a57bbb314a4e2edaf4c8028
Lint Action <lint-action@samuelmeuli.com>
2025-02-28 20:14:41 +0000

    style: fix linting issues met lint-action

--- test.ts
+++ test.ts
@@ -1,9 +1,8 @@
 async function test_Function() {
-    return 1+1;
+    return 1 + 1;
 }
 
 for (let index = 0; index < 100; index++) {
-        await test_Function();
-    const a = "test";
+    await test_Function();
+    const a = 'test';
 }
-
