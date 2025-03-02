89fff0884100f53dfa850e4384abdac08ff7beb8
Laure Jablonski <laure.jablonski@ugent.be>
2025-03-02 09:47:38 +0100

    feat(frontend): basiscomponenten en functionaliteit aanwezig in login pagina

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,10 +1,34 @@
 <script setup lang="ts">
+    import { useRoute } from "vue-router";
+    import dwengoLogo from "../../../assets/img/dwengo-groen-zwart.svg";
+
+    const route = useRoute();
+
+    const userId = 1;
 </script>
 
 <template>
-    <main></main>
+    <main>
+        <img
+            class="dwengo_logo"
+            :src="dwengoLogo"
+        />
+        <div>
+            <p>login</p>
+            <v-btn
+                :to="`/teacher/${userId}`"
+                append-icon="$vuetify"
+            >
+                teacher
+            </v-btn>
+            <v-btn
+                :to="`/student/${userId}`"
+                append-icon="$vuetify"
+            >
+                student
+            </v-btn>
+        </div>
+    </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
