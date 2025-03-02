7fcde1913bb595af0f0f73b332cf075828a860d5
Laure Jablonski <laure.jablonski@ugent.be>
2025-03-02 10:04:39 +0100

    style: achtergrond en frame op hun plaats, elementen onder elkaar

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,34 +1,69 @@
 <script setup lang="ts">
-    import { useRoute } from "vue-router";
     import dwengoLogo from "../../../assets/img/dwengo-groen-zwart.svg";
-
-    const route = useRoute();
 
     const userId = 1;
 </script>
 
 <template>
     <main>
-        <img
-            class="dwengo_logo"
-            :src="dwengoLogo"
-        />
-        <div>
-            <p>login</p>
-            <v-btn
-                :to="`/teacher/${userId}`"
-                append-icon="$vuetify"
-            >
-                teacher
-            </v-btn>
-            <v-btn
-                :to="`/student/${userId}`"
-                append-icon="$vuetify"
-            >
-                student
-            </v-btn>
+        <div class="login_background">
+            <ul>
+                <img
+                    class="dwengo_logo"
+                    :src="dwengoLogo"
+                />
+                <div class="container">
+                    <ul>
+                        <li>login</li>
+                        <li>
+                            <v-btn
+                                :to="`/teacher/${userId}`"
+                                append-icon="$vuetify"
+                            >
+                                teacher
+                            </v-btn>
+                        </li>
+                        <li>
+                            <v-btn
+                                :to="`/student/${userId}`"
+                                append-icon="$vuetify"
+                            >
+                                student
+                            </v-btn>
+                        </li>
+                    </ul>
+                </div>
+            </ul>
         </div>
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .login_background {
+        background-color: #f6faf2;
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    img {
+        width: 200px;
+    }
+
+    ul {
+        list-style: none;
+        align-items: center;
+    }
+
+    li {
+        padding: 20px;
+        align-items: center;
+    }
+
+    .container {
+        background-color: white;
+        width: 350px;
+        height: 400px;
+    }
+</style>
