5a7bbf9d9ca3bcd6360a42ec667d035892e9c9db
Joyelle Ndagijimana <njoyelle2@gmail.com>
2025-03-06 13:36:54 +0100

    feat(frontend): 'en' en 'nl' vertalingen zijn nu beschikbaar via de menubalk.

--- frontend/src/i18n/i18n.ts
+++ frontend/src/i18n/i18n.ts
@@ -1,20 +1,21 @@
-import { createI18n } from "vue-i18n";
+import { createI18n } from 'vue-i18n';
 
 // Import translations
-import en from "@/i18n/locales/en.json";
-import nl from "@/i18n/locales/nl.json";
-import fr from "@/i18n/locales/fr.json";
-import de from "@/i18n/locales/de.json";
+import en from "@/i18n/locale/en.json";
+import nl from "@/i18n/locale/nl.json";
+import fr from "@/i18n/locale/fr.json";
+import de from "@/i18n/locale/de.json";
+
+const savedLocale = localStorage.getItem('user-lang') || 'en';
 
 const i18n = createI18n({
-    //legacy: false,
-    locale: "en",
-    fallbackLocale: "en",
+    locale: savedLocale,
+    fallbackLocale: 'en',
     messages: {
-        en: { ...en },
-        nl: { ...nl },
-        fr: { ...fr },
-        de: { ...de },
+        en: en,
+        nl: nl,
+        fr: fr,
+        de: de,
     },
 });
 
