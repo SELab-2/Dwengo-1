41cea9bbf0ea8b005a92f7a2f830e0ec1644cfd9
Laure Jablonski <laure.jablonski@ugent.be>
2025-03-07 12:03:23 +0100

    fix: Teacher entity had geen commit (vorige was verkeerde file)

--- backend/src/entities/users/teacher.entity.ts
+++ backend/src/entities/users/teacher.entity.ts
@@ -1,11 +1,24 @@
 import { Collection, Entity, ManyToMany } from '@mikro-orm/core';
 import { User } from './user.entity.js';
 import { Class } from '../classes/class.entity.js';
+import { TeacherRepository } from '../../data/users/teacher-repository.js';
 
-@Entity()
+@Entity({
+    repository: () => {
+        return TeacherRepository;
+    },
+})
 export class Teacher extends User {
     @ManyToMany(() => {
         return Class;
     })
     classes!: Collection<Class>;
+
+    constructor(
+        public username: string,
+        public firstName: string,
+        public lastName: string
+    ) {
+        super();
+    }
 }
