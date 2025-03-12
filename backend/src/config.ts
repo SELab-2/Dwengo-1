f806219777265a5370e088615ddd71d93ec56006
Laure Jablonski <laure.jablonski@ugent.be>
2025-03-12 22:34:32 +0100

    fix: local env bestand teruggezet en logging teruggezet

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -6,5 +6,5 @@ export const DWENGO_API_BASE = getEnvVar(EnvVars.LearningContentRepoApiBaseUrl);
 export const FALLBACK_LANG = getEnvVar(EnvVars.FallbackLanguage);
 
 // Logging
-export const LOG_LEVEL: string = 'info';
+export const LOG_LEVEL: string = 'development' === process.env.NODE_ENV ? 'debug' : 'info';
 export const LOKI_HOST: string = process.env.LOKI_HOST || 'http://localhost:3102';
