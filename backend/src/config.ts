897549a1e09884e47d573d6a0b21b35a86c87571
Laure Jablonski <laure.jablonski@ugent.be>
2025-03-12 21:28:46 +0100

    fix: lange foutmeldingen en debug logging afgezet

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,9 +1,10 @@
 import { EnvVars, getEnvVar } from './util/envvars.js';
 
+
 // API
 export const DWENGO_API_BASE = getEnvVar(EnvVars.LearningContentRepoApiBaseUrl);
 export const FALLBACK_LANG = getEnvVar(EnvVars.FallbackLanguage);
 
 // Logging
-export const LOG_LEVEL: string = 'development' === process.env.NODE_ENV ? 'debug' : 'info';
+export const LOG_LEVEL: string = 'info';
 export const LOKI_HOST: string = process.env.LOKI_HOST || 'http://localhost:3102';
