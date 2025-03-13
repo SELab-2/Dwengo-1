e3e1fc3f05d2d817e958471e329d32c6b2559a76
Gabriellvl <vanlangenhovefga@gmail.com>
2025-03-13 15:30:07 +0100

    fix: gebruik create question functie

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,6 +5,10 @@
 // Load .env file
 // Dotenv.config();
 
+import {Language} from "./entities/content/language.js";
+
 export const DWENGO_API_BASE = 'https://dwengo.org/backend/api';
 
-export const FALLBACK_LANG = 'nl';
+export const FALLBACK_LANG: Language = Language.Dutch;
+
+export const FALLBACK_SEQ_NUM = 1;
