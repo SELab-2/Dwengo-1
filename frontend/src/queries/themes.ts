fbfe54879a6eaf6b41f6c9e441f5f31faa5302f9
Lint Action <lint-action@samuelmeuli.com>
2025-03-25 21:45:58 +0000

    style: fix linting issues met ESLint

--- frontend/src/queries/themes.ts
+++ frontend/src/queries/themes.ts
@@ -4,22 +4,18 @@ import {type MaybeRefOrGetter, toValue} from "vue";
 
 const themeController = getThemeController();
 
-export const useThemeQuery = (language: MaybeRefOrGetter<string>) => {
-    return useQuery({
+export const useThemeQuery = (language: MaybeRefOrGetter<string>) => useQuery({
         queryKey: ['themes', language],
         queryFn: () => {
             const lang = toValue(language);
             return themeController.getAll(lang);
         },
-        enabled: () => !!toValue(language),
+        enabled: () => Boolean(toValue(language)),
     });
-};
 
-export const useThemeHruidsQuery = (themeKey: string | null) => {
-    return useQuery({
+export const useThemeHruidsQuery = (themeKey: string | null) => useQuery({
         queryKey: ['theme-hruids', themeKey],
         queryFn: () => themeController.getHruidsByKey(themeKey!),
-        enabled: !!themeKey,
+        enabled: Boolean(themeKey),
     });
-};
 
