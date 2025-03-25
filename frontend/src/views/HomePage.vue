bb6034dff8120ab54fd0f9fc363034dc5156ebde
Tibo De Peuter <tibo.depeuter@ugent.be>
2025-03-25 09:46:18 +0100

    refactor(frontend): return-types en functie stijl

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -13,10 +13,10 @@
     ]);
 
     // Logic to change the language of the website to the selected language
-    const changeLanguage = (langCode: string) => {
+    function changeLanguage(langCode: string): void {
         locale.value = langCode;
         localStorage.setItem("user-lang", langCode);
-    };
+    }
 </script>
 
 <template>
