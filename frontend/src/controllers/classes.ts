eb8ac9c3bb8f5d6894a1069cdfb269e3de127960
Adriaan Jacquet <adriaanjacquet@gmail.com>
2025-03-30 17:38:32 +0200

    feat: frontend klas controller aangemaakt

---
+++ frontend/src/controllers/classes.ts
@@ -0,0 +1,33 @@
+import { BaseController } from "./base-controller";
+
+
+export class ClassController extends BaseController {
+    constructor() {
+        super("class");
+    }
+
+    getAll(full = true) {
+        return this.get<{ classes: any[] }>(`/`, { full });
+    }
+
+    getById(id: string) {
+        return this.get<{ class: any }>(`/${id}`);
+    }
+
+    createClass(data: any) {
+        return this.post<{ class: any }>(`/`, data);
+    }
+
+    deleteClass(id: string) {
+        return this.delete<{ class: any }>(`/${id}`);
+    }
+
+    
+    getStudents(id: string, full = true) {
+        return this.get<{ students: any[] }>(`/${id}/students`, { full });
+    }
+
+    getTeacherInvitations(id: string, full = true) {
+        return this.get<{ invitations: any[] }>(`/${id}/teacher-invitations`, { full });
+    }
+}
