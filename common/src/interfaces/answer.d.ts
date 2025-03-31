6c9c8d782dc6aa61a7a5c314530afce70522c7fc
Tibo De Peuter <tibo.depeuter@ugent.be>
2025-03-31 18:40:19 +0200

    refactor(common): AnswerDTO

---
+++ common/src/interfaces/answer.d.ts
@@ -0,0 +1,16 @@
+import { UserDTO } from 'dwengo-1-backend/src/interfaces/user.js'
+import { QuestionDTO } from 'dwengo-1-backend/src/interfaces/question.js'
+
+export interface AnswerDTO {
+    author: UserDTO;
+    toQuestion: QuestionDTO;
+    sequenceNumber: number;
+    timestamp: string;
+    content: string;
+}
+
+export interface AnswerId {
+    author: string;
+    toQuestion: QuestionId;
+    sequenceNumber: number;
+}
