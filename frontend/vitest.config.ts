ae45ba7ca8a7edb60a347ee16827169615795115
Timo De Meyst <timo.demeyst@ugent.be>
2025-04-04 17:31:39 +0200

    actions: frontend test coverage wordt gereport door de workflow

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -9,6 +9,12 @@ export default mergeConfig(
             environment: "jsdom",
             exclude: [...configDefaults.exclude, "e2e/**"],
             root: fileURLToPath(new URL("./", import.meta.url)),
+            coverage: {
+                reporter: ['text', 'json-summary', 'json'],
+                // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+                reportOnFailure: true,
+                exclude: ['**/*config*'],
+              },
         },
     }),
 );
