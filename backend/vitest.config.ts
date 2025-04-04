8766bd40c74aa5d737f5da1bc456dafdb1423947
Timo De Meyst <timo.demeyst@ugent.be>
2025-04-04 18:08:34 +0200

    actions: backend vitest geconfigureerd voor coverage

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -5,5 +5,17 @@ export default defineConfig({
         environment: 'node',
         globals: true,
         testTimeout: 100000,
+        coverage: {
+            reporter: ['text', 'json-summary', 'json'],
+            // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+            reportOnFailure: true,
+            exclude: ['**/*config*'],
+            thresholds: {
+                lines: 60,
+                branches: 60,
+                functions: 60,
+                statements: 60
+              },
+          },
     },
 });
