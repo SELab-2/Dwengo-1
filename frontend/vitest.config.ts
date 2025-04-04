2a0d7e4626a941391380e268571021fc6cb9d0d0
Timo De Meyst <timo.demeyst@ugent.be>
2025-04-04 18:04:14 +0200

    actions: threshold voor coverage op 60% gezet

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -14,6 +14,12 @@ export default mergeConfig(
                 // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
                 reportOnFailure: true,
                 exclude: ['**/*config*'],
+                thresholds: {
+                    lines: 60,
+                    branches: 60,
+                    functions: 60,
+                    statements: 60
+                  },
               },
         },
     }),
