3ca063a72135f84301f4630774b752261e02015b
Timo De Meyst <timo.demeyst@ugent.be>
2025-04-05 11:13:06 +0200

    actions: backend coverage exclude aangepast test bestanden moeten niet gecovered worden Bestanden rechtstreeks onder de src folder moeten ook niet gecovered worden (deze binden heel het project samen maar hebben geen aparte functionaliteit om te testen)

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
             reporter: ['text', 'json-summary', 'json'],
             // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
             reportOnFailure: true,
-            exclude: ['**/*config*'],
+            exclude: ['**/*config*', '**/tests/**', 'src/*.ts'],
             thresholds: {
                 lines: 60,
                 branches: 60,
