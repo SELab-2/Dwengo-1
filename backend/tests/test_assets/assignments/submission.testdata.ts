8a863d8589605dae1ffbf5b0239b45a6a5b33787
Gabriellvl <vanlangenhovefga@gmail.com>
2025-04-12 11:47:58 +0200

    fix: comments back

--- backend/tests/test_assets/assignments/submission.testdata.ts
+++ backend/tests/test_assets/assignments/submission.testdata.ts
@@ -16,13 +16,21 @@ let submission08: Submission;
 export let TEST_SUBMISSION_LIST: Submission[];
 
 export function makeTestSubmissions(em: EntityManager, students: Student[], groups: Group[]): Submission[] {
+    // Group #1 for Assignment #1 in class 'id01'
     submission01 = em.create(Submission, { learningObjectHruid: 'id03', learningObjectLanguage: Language.English, learningObjectVersion: 1, submissionNumber: 1, submitter: students[0], submissionTime: new Date(2025, 2, 20), onBehalfOf: groups[0], content: 'sub1' });
+    // Group #1 for Assignment #1 in class 'id01'
     submission02 = em.create(Submission, { learningObjectHruid: 'id03', learningObjectLanguage: Language.English, learningObjectVersion: 1, submissionNumber: 2, submitter: students[0], submissionTime: new Date(2025, 2, 25), onBehalfOf: groups[0], content: '' });
+    // Group #1 for Assignment #1 in class 'id01'
     submission03 = em.create(Submission, { learningObjectHruid: 'id02', learningObjectLanguage: Language.English, learningObjectVersion: 1, submissionNumber: 1, submitter: students[0], submissionTime: new Date(2025, 2, 20), onBehalfOf: groups[0], content: '' });
+    // Group #1 for Assignment #1 in class 'id01'
     submission04 = em.create(Submission, { learningObjectHruid: 'id02', learningObjectLanguage: Language.English, learningObjectVersion: 1, submissionNumber: 2, submitter: students[0], submissionTime: new Date(2025, 2, 25), onBehalfOf: groups[0], content: '' });
+    // Group #2 for Assignment #1 in class 'id01'
     submission05 = em.create(Submission, { learningObjectHruid: 'id01', learningObjectLanguage: Language.English, learningObjectVersion: 1, submissionNumber: 1, submitter: students[1], submissionTime: new Date(2025, 2, 20), onBehalfOf: groups[1], content: '' });
+    // Group #5 for Assignment #4 in class 'id01'
     submission06 = em.create(Submission, { learningObjectHruid: 'id01', learningObjectLanguage: Language.English, learningObjectVersion: 1, submissionNumber: 2, submitter: students[1], submissionTime: new Date(2025, 2, 25), onBehalfOf: groups[4], content: '' });
+    // Group #4 for Assignment #2 in class 'id02'
     submission07 = em.create(Submission, { learningObjectHruid: 'id01', learningObjectLanguage: Language.English, learningObjectVersion: 1, submissionNumber: 3, submitter: students[3], submissionTime: new Date(2025, 3, 25), onBehalfOf: groups[3], content: '' });
+    // Group #2 for Assignment #1 in class 'id01'
     submission08 = em.create(Submission, { learningObjectHruid: 'id02', learningObjectLanguage: Language.English, learningObjectVersion: 1, submissionNumber: 3, submitter: students[1], submissionTime: new Date(2025, 4, 7), onBehalfOf: groups[1], content: '' });
 
     TEST_SUBMISSION_LIST = [submission01, submission02, submission03, submission04, submission05, submission06, submission07, submission08];
--- backend/tests/test_assets/questions/questions.testdata.ts
+++ backend/tests/test_assets/questions/questions.testdata.ts
@@ -14,11 +14,17 @@ let question06: Question;
 export let TEST_QUESTION_LIST: Question[];
 
 export function makeTestQuestions(em: EntityManager, students: Student[], groups: Group[]): Question[] {
+    // Group #1 for Assignment #1 in class 'id01'
     question01 = em.create(Question, { learningObjectLanguage: Language.English, learningObjectVersion: 1, learningObjectHruid: 'id05', inGroup: groups[0], sequenceNumber: 1, author: students[0], timestamp: new Date(), content: 'question' });
+    // Group #1 for Assignment #1 in class 'id01'
     question02 = em.create(Question, { learningObjectLanguage: Language.English, learningObjectVersion: 1, learningObjectHruid: 'id05', inGroup: groups[0], sequenceNumber: 2, author: students[2], timestamp: new Date(), content: 'question' });
+    // Group #1 for Assignment #1 in class 'id01'
     question03 = em.create(Question, { learningObjectLanguage: Language.English, learningObjectVersion: 1, learningObjectHruid: 'id04', inGroup: groups[0], sequenceNumber: 1, author: students[0], timestamp: new Date(), content: 'question' });
+    // Group #2 for Assignment #1 in class 'id01'
     question04 = em.create(Question, { learningObjectLanguage: Language.English, learningObjectVersion: 1, learningObjectHruid: 'id01', inGroup: groups[1], sequenceNumber: 1, author: students[1], timestamp: new Date(), content: 'question' });
+    // Group #2 for Assignment #1 in class 'id01'
     question05 = em.create(Question, { learningObjectLanguage: Language.English, learningObjectVersion: 1, learningObjectHruid: 'id05', inGroup: groups[1], sequenceNumber: 3, author: students[1], timestamp: new Date(), content: 'question' });
+    // Group #4 for Assignment #2 in class 'id02'
     question06 = em.create(Question, { learningObjectLanguage: Language.English, learningObjectVersion: 1, learningObjectHruid: 'id05', inGroup: groups[3], sequenceNumber: 4, author: students[2], timestamp: new Date(), content: 'question' });
 
     TEST_QUESTION_LIST = [question01, question02, question03, question04, question05, question06];
