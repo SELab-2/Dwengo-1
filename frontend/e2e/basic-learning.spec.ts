b67fcf19d4195b3c27bc96fc3533996240e1a6bf
Tibo De Peuter <tibo.depeuter@ugent.be>
2025-04-13 19:52:24 +0200

    test: Learning path filters

--- frontend/e2e/basic-learning.spec.ts
+++ frontend/e2e/basic-learning.spec.ts
@@ -1,5 +1,12 @@
 import { test, expect } from './fixtures.js';
 
-test('myTest', async ({ page }) => {
-    await expect(page).toHaveURL('/');
+test('Users can filter', async ({ page }) => {
+    await page.goto('/user');
+
+    await page.getByRole('combobox').filter({ hasText: 'Select a themeAll' }).locator('i').click();
+    await page.getByText('Nature and climate').click();
+    await page.getByRole('combobox').filter({ hasText: 'Select ageAll agesSelect age' }).locator('i').click();
+    await page.getByText('and older').click();
+
+    await expect(page.getByRole('link', { name: 'AI and Climate Students in' })).toBeVisible();
 });
