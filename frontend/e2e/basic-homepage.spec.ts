d53d1a018c8f1f3d6c44ec377644aedbd5a1befb
Tibo De Peuter <tibo.depeuter@ugent.be>
2025-04-13 14:26:22 +0200

    test: Teacher and student can sign in

--- frontend/e2e/basic-homepage.spec.ts
+++ frontend/e2e/basic-homepage.spec.ts
@@ -15,3 +15,41 @@ test('User can pick their language', async ({ page }) => {
     await expect(page.locator('h1')).toContainText('Our strengths');
     await expect(page.getByRole('heading', { name: 'Innovative' })).toBeVisible();
 });
+
+test('Teacher can sign in', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByRole('link', { name: 'log in' })).toBeVisible();
+    await page.getByRole('link', { name: 'log in' }).click();
+
+    await expect(page.getByRole('button', { name: 'teacher' })).toBeVisible();
+    await page.getByRole('button', { name: 'teacher' }).click();
+
+    await expect(page.getByText('teacher')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Sign In' })).toBeVisible();
+
+    await page.getByRole('textbox', { name: 'Username or email' }).fill('testleerkracht1');
+    await page.getByRole('textbox', { name: 'Password' }).fill('password');
+    await page.getByRole('button', { name: 'Sign In' }).click();
+
+    await expect(page.getByRole('link', { name: 'Dwengo logo teacher' })).toBeVisible();
+    await expect(page.getByRole('button').nth(1)).toBeVisible();
+});
+
+test('Student can sign in', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByRole('link', { name: 'log in' })).toBeVisible();
+    await page.getByRole('link', { name: 'log in' }).click();
+
+    await expect(page.getByRole('button', { name: 'student' })).toBeVisible();
+    await page.getByRole('button', { name: 'student' }).click();
+
+    await expect(page.getByText('student')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Sign In' })).toBeVisible();
+
+    await page.getByRole('textbox', { name: 'Username or email' }).fill('testleerling1');
+    await page.getByRole('textbox', { name: 'Password' }).fill('password');
+    await page.getByRole('button', { name: 'Sign In' }).click();
+
+    await expect(page.getByRole('link', { name: 'Dwengo logo student' })).toBeVisible();
+    await expect(page.getByRole('button').nth(1)).toBeVisible();
+});
