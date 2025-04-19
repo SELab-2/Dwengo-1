c2f3a6169a56555bc91f0653f837295b25cb803d
Gerald Schmittinger <gerald.schmittinger@ugent.be>
2025-04-19 10:03:54 +0200

    Backend geeft nu een 401 ipv. een 500 terug voor de 'expired jwt' fout

---
+++ backend/src/exceptions/has-status-code.ts
@@ -0,0 +1,6 @@
+export interface HasStatusCode {
+    status: number
+}
+export function hasStatusCode(err: unknown): err is HasStatusCode {
+    return typeof err === 'object' && err !== null && 'status' in err && typeof (err as HasStatusCode)?.status === 'number';
+}
--- backend/src/middleware/error-handling/error-handler.ts
+++ backend/src/middleware/error-handling/error-handler.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { getLogger, Logger } from '../../logging/initalize.js';
-import { ExceptionWithHttpState } from '../../exceptions/exception-with-http-state.js';
+import { hasStatusCode } from '../../exceptions/has-status-code.js';
 
 const logger: Logger = getLogger();
 
 export function errorHandler(err: unknown, _req: Request, res: Response, _: NextFunction): void {
-    if (err instanceof ExceptionWithHttpState) {
+    if (hasStatusCode(err)) {
         logger.warn(`An error occurred while handling a request: ${err} (-> HTTP ${err.status})`);
         res.status(err.status).json(err);
     } else {
