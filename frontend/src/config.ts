2d5988552fb91352026b43e6e76330ba6a3b55b7
Tibo De Peuter <tibo.depeuter@ugent.be>
2025-04-20 20:29:34 +0200

    chore(frontend): Configureerbare API URL

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,8 +1,16 @@
 export const apiConfig = {
-    baseUrl:
-        window.location.hostname === "localhost" && !(window.location.port === "80" || window.location.port === "")
-            ? "http://localhost:3000/api"
-            : window.location.origin + "/api",
+    baseUrl: ((): string => {
+        if (import.meta.env.VITE_API_BASE_URL) {
+            return import.meta.env.VITE_API_BASE_URL;
+        }
+
+        if (window.location.hostname === "localhost" && !(window.location.port === "80" || window.location.port === "")) {
+            return "http://localhost:3000/api";
+        }
+
+        // Fallback to the current origin with "/api" suffix
+        return `${window.location.origin}/api`;
+    })(),
 };
 
 export const loginRoute = "/login";
