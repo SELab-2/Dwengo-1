a3ae3db81d093dbc4ef403c098ffd914efb8f6ca
laurejablonski <laure.jablonski@ugent.be>
2025-04-21 23:45:50 +0200

    feat: compontenten voor q&a

---
+++ frontend/src/components/QandA.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import type { QuestionDTO } from "@dwengo-1/common/interfaces/question";
+    import SingleQuestion from "./SingleQuestion.vue";
+
+    defineProps<{
+        questions: QuestionDTO[];
+    }>();
+</script>
+<template>
+    <div class="space-y-4">
+        <div
+            v-for="(question) in questions"
+            :key="(question.sequenceNumber, question.content)"
+            class="border rounded-2xl p-4 shadow-sm bg-white"
+        >
+            <SingleQuestion :question="question"></SingleQuestion>
+
+        
+        </div>
+    </div>
+</template>
+<style scoped></style>
