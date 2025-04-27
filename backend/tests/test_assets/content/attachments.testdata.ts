db9b9ce2cd0e68f7bd0b9761ae29cce5b9b6ec23
laurejablonski <laure.jablonski@ugent.be>
2025-04-27 11:04:17 +0200

    test: students data

--- backend/tests/test_assets/content/attachments.testdata.ts
+++ backend/tests/test_assets/content/attachments.testdata.ts
@@ -4,6 +4,7 @@ import { testLearningObject01 } from './learning-objects.testdata';
 import { LearningObject } from '../../../src/entities/content/learning-object.entity';
 
 export function makeTestAttachments(em: EntityManager): Attachment[] {
+    // prevent duplicate insertion
     const lo = em.merge(LearningObject, testLearningObject01);
 
     attachment01 = em.create(Attachment, {
