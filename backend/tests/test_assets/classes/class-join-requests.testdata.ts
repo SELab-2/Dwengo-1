b71b901f9e2ff682ff919837fb2e8a17bc5a5c51
laurejablonski <laure.jablonski@ugent.be>
2025-04-27 10:01:23 +0200

    fix: link + exporteren classjoinrequests

--- backend/tests/test_assets/classes/class-join-requests.testdata.ts
+++ backend/tests/test_assets/classes/class-join-requests.testdata.ts
@@ -3,31 +3,54 @@ import { ClassJoinRequest } from '../../../src/entities/classes/class-join-reque
 import { Student } from '../../../src/entities/users/student.entity';
 import { Class } from '../../../src/entities/classes/class.entity';
 import { ClassStatus } from '@dwengo-1/common/util/class-join-request';
+import { getPinkFloyd, getSmashingPumpkins, getTool } from '../users/students.testdata';
+import { getClass02, getClass03 } from './classes.testdata';
 
 export function makeTestClassJoinRequests(em: EntityManager, students: Student[], classes: Class[]): ClassJoinRequest[] {
-    const classJoinRequest01 = em.create(ClassJoinRequest, {
-        requester: students[4],
-        class: classes[1],
+    classJoinRequest01 = em.create(ClassJoinRequest, {
+        requester: getPinkFloyd(),
+        class: getClass02(),
         status: ClassStatus.Open,
     });
 
-    const classJoinRequest02 = em.create(ClassJoinRequest, {
-        requester: students[2],
-        class: classes[1],
+    classJoinRequest02 = em.create(ClassJoinRequest, {
+        requester: getTool(),
+        class: getClass02(),
         status: ClassStatus.Open,
     });
 
-    const classJoinRequest03 = em.create(ClassJoinRequest, {
-        requester: students[4],
-        class: classes[2],
+    classJoinRequest03 = em.create(ClassJoinRequest, {
+        requester: getPinkFloyd(),
+        class: getClass03(),
         status: ClassStatus.Open,
     });
 
-    const classJoinRequest04 = em.create(ClassJoinRequest, {
-        requester: students[3],
-        class: classes[2],
+    classJoinRequest04 = em.create(ClassJoinRequest, {
+        requester: getSmashingPumpkins(),
+        class: getClass03(),
         status: ClassStatus.Open,
     });
 
     return [classJoinRequest01, classJoinRequest02, classJoinRequest03, classJoinRequest04];
+}
+
+let classJoinRequest01: ClassJoinRequest;
+let classJoinRequest02: ClassJoinRequest;
+let classJoinRequest03: ClassJoinRequest;
+let classJoinRequest04: ClassJoinRequest;
+
+export function getClassJoinRequest01(): ClassJoinRequest{
+    return classJoinRequest01;
+}
+
+export function getClassJoinRequest02(): ClassJoinRequest{
+    return classJoinRequest02;
+}
+
+export function getClassJoinRequest03(): ClassJoinRequest{
+    return classJoinRequest03;
+}
+
+export function getClassJoinRequest04(): ClassJoinRequest{
+    return classJoinRequest04;
 }
