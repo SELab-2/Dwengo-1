2f28b801807c92816c34b0918f09f716370d4c9e
Lint Action <lint-action@samuelmeuli.com>
2025-05-06 07:32:26 +0000

    style: fix linting issues met Prettier

--- frontend/src/views/learning-paths/LearningPathSearchPage.vue
+++ frontend/src/views/learning-paths/LearningPathSearchPage.vue
@@ -18,8 +18,16 @@
 
 <template>
     <v-container class="search-page-container">
-        <v-row justify="center" class="mb-6">
-            <v-col cols="12" sm="8" md="6" lg="4">
+        <v-row
+            justify="center"
+            class="mb-6"
+        >
+            <v-col
+                cols="12"
+                sm="8"
+                md="6"
+                lg="4"
+            >
                 <learning-path-search-field class="search-field" />
             </v-col>
         </v-row>
@@ -33,7 +41,10 @@
                     <learning-paths-grid :learning-paths="data" />
                 </using-query-result>
 
-                <div v-if="!query" class="empty-state-container">
+                <div
+                    v-if="!query"
+                    class="empty-state-container"
+                >
                     <v-empty-state
                         icon="mdi-magnify"
                         :title="t('enterSearchTerm')"
@@ -46,12 +57,12 @@
 </template>
 
 <style scoped>
-.search-page-container {
-    padding-top: 40px;
-    padding-bottom: 40px;
-}
+    .search-page-container {
+        padding-top: 40px;
+        padding-bottom: 40px;
+    }
 
-.search-field {
-    max-width: 100%;
-}
+    .search-field {
+        max-width: 100%;
+    }
 </style>
