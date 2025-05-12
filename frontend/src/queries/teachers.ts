17620434f24ca4ae64efadac8ca0c8318ccb843b
Gabriellvl <vanlangenhovefga@gmail.com>
2025-05-12 23:09:13 +0200

    refactor: delete endpoint back and frontend

--- frontend/src/queries/teachers.ts
+++ frontend/src/queries/teachers.ts
@@ -10,7 +10,6 @@ import {
 import { TeacherController, type TeacherResponse, type TeachersResponse } from "@/controllers/teachers.ts";
 import type { ClassesResponse } from "@/controllers/classes.ts";
 import type { JoinRequestResponse, JoinRequestsResponse, StudentsResponse } from "@/controllers/students.ts";
-import type { QuestionsResponse } from "@/controllers/questions.ts";
 import type { TeacherDTO } from "@dwengo-1/common/interfaces/teacher";
 import { studentJoinRequestQueryKey, studentJoinRequestsQueryKey } from "@/queries/students.ts";
 
@@ -76,17 +75,6 @@ export function useTeacherStudentsQuery(
     return useQuery({
         queryKey: computed(() => teacherStudentsQueryKey(toValue(username)!, toValue(full))),
         queryFn: async () => teacherController.getStudents(toValue(username)!, toValue(full)),
-        enabled: () => Boolean(toValue(username)),
-    });
-}
-
-export function useTeacherQuestionsQuery(
-    username: MaybeRefOrGetter<string | undefined>,
-    full: MaybeRefOrGetter<boolean> = false,
-): UseQueryReturnType<QuestionsResponse, Error> {
-    return useQuery({
-        queryKey: computed(() => teacherQuestionsQueryKey(toValue(username)!, toValue(full))),
-        queryFn: async () => teacherController.getQuestions(toValue(username)!, toValue(full)),
         enabled: () => Boolean(toValue(username)),
     });
 }
