e6706d1750159f986848f56369b5854ca91ed5cf
Timo De Meyst <timo.demeyst@ugent.be>
2025-05-15 19:27:52 +0200

    feat: het wordt duidelijk gemaakt als er geen vragen zijn

--- frontend/src/components/QandA.vue
+++ frontend/src/components/QandA.vue
@@ -9,12 +9,27 @@
 <template>
     <div class="space-y-4">
         <div
+            v-if="questions.length !=0"
             v-for="question in questions"
             :key="(question.sequenceNumber, question.content)"
             class="border rounded-2xl p-4 shadow-sm bg-white"
         >
             <SingleQuestion :question="question"></SingleQuestion>
         </div>
+        <div v-else>
+            <p class="no-questions">No questions asked yet</p>
+        </div>
     </div>
 </template>
-<style scoped></style>
+<style scoped>
+.no-questions {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40vh;
+    text-align: center;
+    font-size: 18px;
+    color: #666;
+    padding: 0 20px;
+}
+</style>
