5d8c0a3cd223af7df52fdb28670881ef155dacc6
laurejablonski <laure.jablonski@ugent.be>
2025-05-16 12:33:47 +0200

    fix: groepen bij assignments dynamisch

--- backend/tool/seedORM.ts
+++ backend/tool/seedORM.ts
@@ -6,7 +6,7 @@ import { makeTestLearningObjects } from '../tests/test_assets/content/learning-o
 import { makeTestLearningPaths } from '../tests/test_assets/content/learning-paths.testdata';
 import { makeTestClasses } from '../tests/test_assets/classes/classes.testdata';
 import { makeTestAssignemnts } from '../tests/test_assets/assignments/assignments.testdata';
-import { makeTestGroups } from '../tests/test_assets/assignments/groups.testdata';
+import { getTestGroup01, getTestGroup02, getTestGroup03, getTestGroup04, makeTestGroups } from '../tests/test_assets/assignments/groups.testdata';
 import { Group } from '../src/entities/assignments/group.entity';
 import { makeTestTeacherInvitations } from '../tests/test_assets/classes/teacher-invitations.testdata';
 import { makeTestClassJoinRequests } from '../tests/test_assets/classes/class-join-requests.testdata';
@@ -31,23 +31,23 @@ export async function seedORM(orm: MikroORM): Promise<void> {
     const teachers = makeTestTeachers(em);
     const learningObjects = makeTestLearningObjects(em);
     const learningPaths = makeTestLearningPaths(em);
-    const classes = makeTestClasses(em, students, teachers);
-    const assignments = makeTestAssignemnts(em, classes);
+    const classes = makeTestClasses(em);
+    const assignments = makeTestAssignemnts(em);
 
-    const groups = makeTestGroups(em, students, assignments);
+    const groups = makeTestGroups(em);
 
-    assignments[0].groups = new Collection<Group>(groups.slice(0, 3));
-    assignments[1].groups = new Collection<Group>(groups.slice(3, 4));
+    assignments[0].groups = new Collection<Group>([getTestGroup01(), getTestGroup02(), getTestGroup03()])
+    assignments[1].groups = new Collection<Group>([getTestGroup04()]);
 
-    const teacherInvitations = makeTestTeacherInvitations(em, teachers, classes);
-    const classJoinRequests = makeTestClassJoinRequests(em, students, classes);
-    const attachments = makeTestAttachments(em, learningObjects);
+    const teacherInvitations = makeTestTeacherInvitations(em);
+    const classJoinRequests = makeTestClassJoinRequests(em);
+    const attachments = makeTestAttachments(em);
 
     learningObjects[1].attachments = attachments;
 
-    const questions = makeTestQuestions(em, students, groups);
-    const answers = makeTestAnswers(em, teachers, questions);
-    const submissions = makeTestSubmissions(em, students, groups);
+    const questions = makeTestQuestions(em);
+    const answers = makeTestAnswers(em);
+    const submissions = makeTestSubmissions(em);
 
     // Persist all entities
     await em.persistAndFlush([
