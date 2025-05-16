aa2441e7db02547941f3befcf0f81a6703b92ea2
Tibo De Peuter <tibo.depeuter@ugent.be>
2025-05-16 17:15:33 +0200

    fix(frontend): Filter setup

--- frontend/e2e/basic-learning.spec.ts
+++ frontend/e2e/basic-learning.spec.ts
@@ -1,8 +1,16 @@
-import { test, expect } from "./fixtures.js";
+import { test, expect } from "@playwright/test"
 
 test("Users can filter", async ({ page }) => {
-    await page.goto("/user");
+    await page.goto("/")
 
+    // Login
+    await page.getByRole("link", { name: "log in" }).click();
+    await page.getByRole("button", { name: "teacher" }).click();
+    await page.getByRole("textbox", { name: "Username or email" }).fill("testleerkracht1");
+    await page.getByRole("textbox", { name: "Password" }).fill("password");
+    await page.getByRole("button", { name: "Sign In" }).click();
+
+    // Filter
     await page.getByRole("combobox").filter({ hasText: "Select a themeAll" }).locator("i").click();
     await page.getByText("Nature and climate").click();
     await page.getByRole("combobox").filter({ hasText: "Select ageAll agesSelect age" }).locator("i").click();
