81a8ca7b45dafe888050db74e40c9e70722bc9b7
Lint Action <lint-action@samuelmeuli.com>
2025-05-16 09:01:33 +0000

    style: fix linting issues met Prettier

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import SingleAssignment from '@/views/assignments/SingleAssignment.vue';
-import SingleClass from '@/views/classes/SingleClass.vue';
-import SingleDiscussion from '@/views/discussions/SingleDiscussion.vue';
-import NotFound from '@/components/errors/NotFound.vue';
-import CreateAssignment from '@/views/assignments/CreateAssignment.vue';
-import CreateDiscussion from '@/views/discussions/CreateDiscussion.vue';
-import CallbackPage from '@/views/CallbackPage.vue';
-import UserClasses from '@/views/classes/UserClasses.vue';
-import UserAssignments from '@/views/assignments/UserAssignments.vue';
-import LearningPathPage from '@/views/learning-paths/LearningPathPage.vue';
-import LearningPathSearchPage from '@/views/learning-paths/LearningPathSearchPage.vue';
-import UserHomePage from '@/views/homepage/UserHomePage.vue';
-import SingleTheme from '@/views/SingleTheme.vue';
-import LearningObjectView from '@/views/learning-paths/learning-object/LearningObjectView.vue';
-import authService from '@/services/auth/auth-service';
-import OwnLearningContentPage from '@/views/own-learning-content/OwnLearningContentPage.vue';
-import { allowRedirect, Redirect } from '@/utils/redirect.ts';
+import { createRouter, createWebHistory } from "vue-router";
+import SingleAssignment from "@/views/assignments/SingleAssignment.vue";
+import SingleClass from "@/views/classes/SingleClass.vue";
+import SingleDiscussion from "@/views/discussions/SingleDiscussion.vue";
+import NotFound from "@/components/errors/NotFound.vue";
+import CreateAssignment from "@/views/assignments/CreateAssignment.vue";
+import CreateDiscussion from "@/views/discussions/CreateDiscussion.vue";
+import CallbackPage from "@/views/CallbackPage.vue";
+import UserClasses from "@/views/classes/UserClasses.vue";
+import UserAssignments from "@/views/assignments/UserAssignments.vue";
+import LearningPathPage from "@/views/learning-paths/LearningPathPage.vue";
+import LearningPathSearchPage from "@/views/learning-paths/LearningPathSearchPage.vue";
+import UserHomePage from "@/views/homepage/UserHomePage.vue";
+import SingleTheme from "@/views/SingleTheme.vue";
+import LearningObjectView from "@/views/learning-paths/learning-object/LearningObjectView.vue";
+import authService from "@/services/auth/auth-service";
+import OwnLearningContentPage from "@/views/own-learning-content/OwnLearningContentPage.vue";
+import { allowRedirect, Redirect } from "@/utils/redirect.ts";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
