fe397c54e3cfbdf94a04dde63067f651363b8643
Tibo De Peuter <tibo.depeuter@ugent.be>
2025-05-17 15:16:13 +0200

    chore(frontend): QandA i18n

--- frontend/src/components/QandA.vue
+++ frontend/src/components/QandA.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
     import type { QuestionDTO } from "@dwengo-1/common/interfaces/question";
     import SingleQuestion from "./SingleQuestion.vue";
+    import { useI18n } from 'vue-i18n';
+
+    const { t } = useI18n();
 
     defineProps<{
         questions: QuestionDTO[];
@@ -8,16 +11,17 @@
 </script>
 <template>
     <div class="space-y-4">
-        <div
-            v-if="questions.length !=0"
-            v-for="question in questions"
-            :key="(question.sequenceNumber, question.content)"
-            class="border rounded-2xl p-4 shadow-sm bg-white"
-        >
-            <SingleQuestion :question="question"></SingleQuestion>
+        <div v-if="questions.length != 0">
+            <div
+                v-for="question in questions"
+                :key="(question.sequenceNumber, question.content)"
+                class="border rounded-2xl p-4 shadow-sm bg-white"
+            >
+                <SingleQuestion :question="question"></SingleQuestion>
+            </div>
         </div>
         <div v-else>
-            <p class="no-questions">No questions asked yet</p>
+            <p class="no-questions">{{t("no-questions")}}</p>
         </div>
     </div>
 </template>
